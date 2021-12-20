ce7736967951a119266da08afd01e5bad713eb92
Ebireni David <ebirenidavid@gmail.com>
2021-12-20 11:06:27 +0100

    set up mongodb connection and setup nodemon with the main file

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
-require('dotenv').config()
+dotenv.config()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3205
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('connected to mongo db successfully'))
+.catch(err => console.log(err.message))
 
 app.listen(PORT, () => {
     console.log(`running on localhost:${PORT}`)
